c43d952b006b5507aa272c542206d747fdf9a0ac
aprizalabyan <aprizalabyan@gmail.com>
2019-01-09 15:30:02 +0700

    First commit

---
+++ Assets/Scripts/gantiScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gantiScene : MonoBehaviour {
+	
+	[SerializeField] private string namaScene;
+
+	void OnTriggerEnter(Collider col){
+		
+			SceneManager.LoadScene (namaScene);
+
+	}
+}
